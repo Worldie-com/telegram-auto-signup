ab9f3f43fda948f754d9696bf18e54c2a91da905
Lee Seungmin <lee@seungmin.io>
2020-05-28 03:09:08 +0900

    feat: auto signup

---
+++ index.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const TelegramSignUp = require("./util");
+const axios = require("axios");
+
+// const fivesimApi = axios.create({
+//   baseURL: "https://5sim.net/v1",
+//   headers: {
+//     Authorization: process.env.FIVESIM_API_TOKEN,
+//   },
+// });
+
+// const telegram = new TelegramSignUp(
+//   process.env.TELEGRAM_API_KEY,
+//   process.env.TELEGRAM_API_HASH,
+//   true
+// );
+
+// const createUser = async () => {
+//   try {
+//     const {
+//       data: { id, phone },
+//     } = await fivesimApi.get("/user/buy/activation/cambodia/any/telegram");
+
+//     const firstName = "Seungmin";
+//     const lastName = "Lee";
+
+//     const phoneCodeHash = await telegram.sendCode(phone);
+//     console.log("Phone Code Hash: " + phoneCodeHash);
+
+//     const intervalRequest = await setInterval(async () => {
+//       const { data } = await fivesimApi.get(`/user/check/${id}`);
+//       if (!(data.sms === [])) {
+//         const signUp = await telegram.signUp(
+//           phone,
+//           phoneCodeHash,
+//           sms[0].code,
+//           firstName,
+//           lastName
+//         );
+//         console.log(signUp.user);
+//       }
+//     }, 5000);
+
+//     await fivesimApi.get(`/user/cancel/${id}`);
+
+//     clearInterval(intervalRequest);
+
+//     console.log(telegram.getStorage());
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// for (var i = 0; i < 6; ) {
+//   createUser();
+//   i++;
+// }
+
+console.log(process.env);
---
+++ util.js
@@ -0,0 +1,56 @@
+const { MTProto } = require("telegram-mtproto");
+
+const LAYER = 57;
+const INIT_CONNECTION = 0x69796de9;
+
+const API_ID = 197316;
+const API_HASH = "f5a841647e17ae9da32b9483d4304e45";
+
+class TelegramSignUp {
+  constructor(api_id, api_hash, dev_server) {
+    this._apiId = api_id;
+    this._apiHash = api_hash;
+
+    // eslint-disable-next-line new-cap
+    this._client = MTProto({
+      server: {
+        dev: dev_server,
+      },
+      api: {
+        layer: LAYER,
+        initConnection: INIT_CONNECTION,
+        api_id,
+      },
+    });
+  }
+
+  async sendCode(phone_number) {
+    const { phone_code_hash } = await this._client("auth.sendCode", {
+      phone_number,
+      current_number: false,
+      api_id: this._apiId,
+      api_hash: this._apiHash,
+    });
+
+    return phone_code_hash;
+  }
+
+  signUp(phone_number, phone_code_hash, phone_code, first_name, last_name) {
+    const params = {
+      phone_number,
+      phone_code_hash,
+      phone_code,
+      first_name,
+    };
+
+    if (last_name) params.last_name = last_name;
+
+    return this._client("auth.signUp", params);
+  }
+
+  getStorage() {
+    return this._client.storage.store;
+  }
+}
+
+module.exports = TelegramSignUp;
